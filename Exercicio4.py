ab0b424567064be54fb90aa96a3fef3b16457813
Bernardo Almeida <99094270+BernKing@users.noreply.github.com>
2023-02-21 19:48:44 +0000

    Update Exercicio4.py

--- Exercicio4.py
+++ Exercicio4.py
@@ -6,6 +6,23 @@
 #Exemplo: 16548973 bytes = 15,78 MBytes.
 # 1mega = 1024 * 1024 bytes
 
-bytes = int(input("Introduza um valor em bytes:"))
+bytes=float(input("Insira o valor em bytes:"))
+if bytes <= 1024:
+    print(bytes, " bytes =", bytes, "bytes ")
+else:
+    if bytes <= 10242:
+        kilo=bytes/1024
+        print(bytes, " bytes =", kilo, " KB ")
+    else:
+        if bytes <= 1024  3:
+            mega = bytes / (10242)
+            print(bytes, " bytes =", mega, "MB ")
+        else:
+            if bytes <= 10244:
+                giga = bytes / (10243)
+                print(bytes, " bytes =", giga, "GB ")
+            else:
+                tera = bytes / (1024 4)
+                print(bytes, " bytes =", tera, "TB ")
 
 
